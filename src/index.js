4e5db725933c20226cf0b830fe838c8979b29770
mdjacksparrow <45224729+mdjacksparrow@users.noreply.github.com>
2020-01-18 13:47:00 +0530

    Learn how to use ReactDOM.render(WHAT IS SHOW, WHEN TO SHOW), CB;

---
+++ src/index.js
@@ -0,0 +1,15 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+ReactDOM.render(
+  <div>
+    <h1>My favorites</h1>
+    <ul>
+      <li>Egg</li>
+      <li>Fried Rice</li>
+      <li>Chicken</li>
+      <li>Movie watching</li>
+    </ul>
+  </div>,
+  document.getElementById("root")
+);
