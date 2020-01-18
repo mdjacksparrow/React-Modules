11fceed82164e14ec7d11056421228b6950bf2ad
mdjacksparrow <45224729+mdjacksparrow@users.noreply.github.com>
2020-01-18 13:57:02 +0530

    Inject js constent to html embedded elements like ejs

--- src/index.js
+++ src/index.js
@@ -1,15 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
+const name = "Jack";
+const luckyNumber = 2;
+
 ReactDOM.render(
   <div>
-    <h1>My favorites</h1>
-    <ul>
-      <li>Egg</li>
-      <li>Fried Rice</li>
-      <li>Chicken</li>
-      <li>Movie watching</li>
-    </ul>
+    <h1>Hello {name}!</h1>
+    <p>My lucky number is {luckyNumber}</p>
   </div>,
   document.getElementById("root")
 );
